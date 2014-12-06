5e7a81ec1c5949e9f7ce780eb92544193b0c06e2
Ivan Kurochkin <ivan.kurochkin@gmail.com>
2014-12-06 12:29:23 +0300

    add sample

---
+++ CachingCollections/CachingCollections/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using CachingCollections;
+
+namespace ConsoleApplication17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to eat memory");
+            Console.ReadKey();
+            Console.WriteLine("wait removing from cache");
+            Task.Factory.StartNew(() =>
+            {
+                var cachedItem = new CachedItem();
+                RuntimeCache.Add("cachedItem", cachedItem);
+                Console.WriteLine("Added to cache");
+            });
+            
+            Console.ReadKey();
+        }
+    }
+    
+    class CachedItem : IDisposable
+    {
+
+        public CachedItem()
+        {
+            _bigArray = new byte[356735678];
+            Console.WriteLine("Obj size: {0} bytes", _bigArray.Length);
+        }
+
+        private byte[] _bigArray;
+        
+
+        public void Dispose()
+        {
+            _bigArray = null; //важно удалить ссылку на массив, иначе после сборки мусора память не освободится
+            Console.WriteLine("CachedItem dispose called;");
+        }
+    }
+}
---
+++ CachingCollections/CachingCollections/RuntimeCache.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Caching;
+
+namespace CachingCollections
+{
+    static class RuntimeCache
+    {
+        private static readonly CacheItemPolicy policy = new CacheItemPolicy
+        {
+            SlidingExpiration = TimeSpan.FromSeconds(5),
+            Priority = CacheItemPriority.Default,
+            RemovedCallback =
+                arg =>
+                {
+                    Console.WriteLine("Removing from cache....");
+
+                    var disposable = arg.CacheItem.Value as IDisposable;
+                    if (disposable != null)
+                        disposable.Dispose();
+
+                    Console.WriteLine("Start collection....");
+
+                    GC.Collect(); //не обязательно вызывать, память освободится при следующем вызове сборки мусора. Вызов может выполняться долго
+                    Console.WriteLine("Collected.");
+                }
+        };
+
+
+        public static void Add(string key, object value)
+        {
+            MemoryCache.Default.Add(key, value, policy);
+        }
+    }
+}

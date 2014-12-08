8ab7084cf9b7dd99b6384b409d1332818169187b
Ivan Kurochkin <ivan.kurochkin@gmail.com>
2014-12-08 14:17:41 +0300

    Add memory metric

--- CachingCollections/CachingCollections/Program.cs
+++ CachingCollections/CachingCollections/Program.cs
@@ -7,6 +7,8 @@ namespace CachingCollections
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Total Memory: {0:n0} bytes", GC.GetTotalMemory(false));
+
             Console.WriteLine("Press any key to eat memory");
             Console.ReadKey();
             Console.WriteLine("wait removing from cache");
@@ -15,28 +17,29 @@ namespace CachingCollections
                 var cachedItem = new CachedItem();
                 RuntimeCache.Add("cachedItem", cachedItem);
                 Console.WriteLine("Added to cache");
+                Console.WriteLine("Total Memory: {0:n0} bytes", GC.GetTotalMemory(false));
             });
-            
+
             Console.ReadKey();
         }
     }
-    
+
     class CachedItem : IDisposable
     {
 
         public CachedItem()
         {
             _bigArray = new byte[356735678];
-            Console.WriteLine("Obj size: {0} bytes", _bigArray.Length);
+            //Console.WriteLine("Obj size: {0} bytes", _bigArray.Length);
         }
 
         private byte[] _bigArray;
-        
+
 
         public void Dispose()
         {
             _bigArray = null; //важно удалить ссылку на массив, иначе после сборки мусора память не освободится (проверено экспериментально)
-            Console.WriteLine("CachedItem dispose called;");
+            Console.WriteLine("CachedItem dispose called.");
         }
     }
 }
--- CachingCollections/CachingCollections/RuntimeCache.cs
+++ CachingCollections/CachingCollections/RuntimeCache.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Runtime.Caching;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CachingCollections
 {
@@ -9,22 +11,31 @@ namespace CachingCollections
         {
             SlidingExpiration = TimeSpan.FromSeconds(5),
             Priority = CacheItemPriority.Default,
-            RemovedCallback =
-                arg =>
-                {
-                    Console.WriteLine("Removing from cache....");
-
-                    var disposable = arg.CacheItem.Value as IDisposable;
-                    if (disposable != null)
-                        disposable.Dispose();
-
-                    Console.WriteLine("Start collection....");
-
-                    GC.Collect(); //не обязательно вызывать, память освободится при следующем вызове сборки мусора. Вызов может выполняться долго
-                    Console.WriteLine("Collected.");
-                }
+            RemovedCallback = RemoveCallback
+              
         };
 
+        private static void RemoveCallback(CacheEntryRemovedArguments arg)
+        {
+            Console.WriteLine("Removing from cache....");
+
+            var disposable = arg.CacheItem.Value as IDisposable;
+            if (disposable != null)
+                disposable.Dispose();
+
+        
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Start collection....");
+                GC.Collect(); //не обязательно вызывать, память освободится при следующем вызове сборки мусора. Вызов может выполняться долго
+                Console.WriteLine("Collected.");
+                Console.WriteLine("Total Memory: {0:n0} bytes", GC.GetTotalMemory(false));
+            });
+
+           
+
+        }
 
         public static void Add(string key, object value)
         {
